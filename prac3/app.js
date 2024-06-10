6ca2cc8e8dc6b2010d156d43e8b6846fd0af0652
AnukulPr1me <anukul_chandra@outlook.com>
2024-06-10 07:12:32 +0530

    rest api

--- prac3/app.js
+++ prac3/app.js
@@ -25,11 +25,7 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  const { error } = schema.validate(req.body);
+  const { error } = validateCourse(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
@@ -44,7 +40,33 @@ app.put("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
     return res.status(404).send("The course with the given ID was not found.");
+
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
 });
+
+app.delete("/api/courses/:id", (req,res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is running on port ${port}`));
